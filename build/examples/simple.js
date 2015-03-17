b565e2280ea6801710e3b0f3a37b8e50dc33d800
yiminghe <yiminghe@gmail.com>
2015-03-17 18:19:00 +0800

    update examples at 1426587540702

--- build/examples/simple.js
+++ build/examples/simple.js
@@ -1,27 +1,21 @@
 webpackJsonp([0,1],[
 /* 0 */
-/*!********************!*\
-  !*** multi simple ***!
-  \********************/
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(/*! ./examples/simple.js */1);
+	module.exports = __webpack_require__(1);
 
 
 /***/ },
 /* 1 */
-/*!****************************!*\
-  !*** ./examples/simple.js ***!
-  \****************************/
 /***/ function(module, exports, __webpack_require__) {
 
 	/** @jsx React.DOM */
 	function onChange(key) {
 	  console.log(key + ' changed!');
 	}
-	__webpack_require__(/*! rc-tabs/assets/bootstrap.css */ 4);
-	var React = __webpack_require__(/*! react */ 2);
-	var Tabs = __webpack_require__(/*! rc-tabs */ 3);
+	__webpack_require__(4);
+	var React = __webpack_require__(2);
+	var Tabs = __webpack_require__(3);
 	var TabPane = Tabs.TabPane;
 	React.render(React.createElement("div", null, 
 	  React.createElement("h1", null, "Simple Tabs"), 
@@ -63,45 +57,36 @@ webpackJsonp([0,1],[
 
 /***/ },
 /* 2 */
-/*!************************!*\
-  !*** external "React" ***!
-  \************************/
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = React;
 
 /***/ },
 /* 3 */
-/*!******************!*\
-  !*** ./index.js ***!
-  \******************/
 /***/ function(module, exports, __webpack_require__) {
 
 	
-	var Tabs = __webpack_require__(/*! ./lib/Tabs */ 6);
-	
+	var Tabs = __webpack_require__(6);
+
 	module.exports = Tabs;
 
 
 /***/ },
 /* 4 */
-/*!******************************!*\
-  !*** ./assets/bootstrap.css ***!
-  \******************************/
 /***/ function(module, exports, __webpack_require__) {
 
 	// style-loader: Adds some css to the DOM by adding a <style> tag
-	
+
 	// load the styles
-	var content = __webpack_require__(/*! !./~/css-loader!./assets/bootstrap.css */ 5);
+	var content = __webpack_require__(5);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(/*! ./~/style-loader/addStyles.js */ 7)(content, {});
+	var update = __webpack_require__(7)(content, {});
 	// Hot Module Replacement
 	if(false) {
 		// When the styles change, update the <style> tags
-		module.hot.accept("!!/Users/yiminghe/code/react-components/tabs/node_modules/css-loader/index.js!/Users/yiminghe/code/react-components/tabs/assets/bootstrap.css", function() {
-			var newContent = require("!!/Users/yiminghe/code/react-components/tabs/node_modules/css-loader/index.js!/Users/yiminghe/code/react-components/tabs/assets/bootstrap.css");
+		module.hot.accept("!!/Users/yiminghe/code/react-components/tabs/node_modules/rc-tools/node_modules/css-loader/index.js!/Users/yiminghe/code/react-components/tabs/assets/bootstrap.css", function() {
+			var newContent = require("!!/Users/yiminghe/code/react-components/tabs/node_modules/rc-tools/node_modules/css-loader/index.js!/Users/yiminghe/code/react-components/tabs/assets/bootstrap.css");
 			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
 			update(newContent);
 		});
@@ -111,25 +96,19 @@ webpackJsonp([0,1],[
 
 /***/ },
 /* 5 */
-/*!*********************************************!*\
-  !*** ./~/css-loader!./assets/bootstrap.css ***!
-  \*********************************************/
 /***/ function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(/*! ./~/css-loader/cssToString.js */ 8)();
+	exports = module.exports = __webpack_require__(8)();
 	exports.push([module.id, ".rc-tabs {\n  outline: none;\n}\n.rc-tabs-nav {\n  box-sizing: border-box;\n  border-bottom: 1px solid #ddd;\n}\n.rc-tabs-tabpane-hidden {\n  display: none;\n}\n.rc-tabs-nav {\n  padding-left: 0;\n  margin-bottom: 0;\n  list-style: none;\n}\n.rc-tabs-nav {\n  margin-top: 0;\n  margin-bottom: 10px;\n}\n.rc-tabs-nav:before,\n.rc-tabs-nav:after {\n  display: table;\n  content: \" \";\n  box-sizing: border-box;\n}\n.rc-tabs-nav:after {\n  clear: both;\n}\n.rc-tabs-nav > li {\n  float: left;\n  margin-bottom: -1px;\n}\n.rc-tabs-nav > li {\n  position: relative;\n  display: block;\n}\n.rc-tabs-nav > li.rc-tabs-tab-active > a,\n.rc-tabs-nav > li.rc-tabs-tab-active > a:hover,\n.rc-tabs-nav > li.rc-tabs-tab-active > a:focus {\n  color: #555;\n  cursor: default;\n  background-color: #fff;\n  border: 1px solid #ddd;\n  border-bottom-color: transparent;\n  text-decoration: none;\n}\n.rc-tabs-nav > li > a {\n  margin-right: 2px;\n  line-height: 1.42857143;\n  border: 1px solid transparent;\n  border-radius: 4px 4px 0 0;\n  text-decoration: none;\n}\n.rc-tabs-nav > li > a {\n  position: relative;\n  display: block;\n  padding: 10px 15px;\n}\n.rc-tabs-nav > li > a:hover {\n  border-color: #eee #eee #ddd;\n  cursor: pointer;\n}\n.rc-tabs-nav > li > a:hover,\n.rc-tabs-nav > li > a:focus {\n  text-decoration: none;\n  background-color: #eee;\n}\n", ""]);
 
 /***/ },
 /* 6 */
-/*!*********************!*\
-  !*** ./lib/Tabs.js ***!
-  \*********************/
 /***/ function(module, exports, __webpack_require__) {
 
 	/** @jsx React.DOM */
-	
-	var React = __webpack_require__(/*! react */ 2);
-	
+
+	var React = __webpack_require__(2);
+
 	var keyCode = {
 	  /**
 	   * LEFT
@@ -148,7 +127,7 @@ webpackJsonp([0,1],[
 	   */
 	  DOWN: 40 // also NUM_SOUTH
 	};
-	
+
 	var ____Class0=React.Component;for(var ____Class0____Key in ____Class0){if(____Class0.hasOwnProperty(____Class0____Key)){TabPane[____Class0____Key]=____Class0[____Class0____Key];}}var ____SuperProtoOf____Class0=____Class0===null?null:____Class0.prototype;TabPane.prototype=Object.create(____SuperProtoOf____Class0);TabPane.prototype.constructor=TabPane;TabPane.__superConstructor__=____Class0;
 	  function TabPane(props) {"use strict";
 	    ____Class0.call(this,props);
@@ -157,8 +136,8 @@ webpackJsonp([0,1],[
 	      prefixCls: props.rootPrefixCls + '-tabpane'
 	    };
 	  }
-	
-	  TabPane.prototype.render=function() {"use strict";
+
+	  Object.defineProperty(TabPane.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
 	    var props = this.props;
 	    var cls = props.active ? '' : this.prefixClsFn('hidden');
 	    cls += ' ' + this.state.prefixCls;
@@ -167,9 +146,9 @@ webpackJsonp([0,1],[
 	        this.props.children
 	      )
 	    );
-	  };
-	
-	
+	  }});
+
+
 	function prefixClsFn() {
 	  var prefixCls = this.state.prefixCls;
 	  var args = Array.prototype.slice.call(arguments, 0);
@@ -177,7 +156,7 @@ webpackJsonp([0,1],[
 	    return prefixCls + '-' + s;
 	  }).join(' ');
 	}
-	
+
 	var ____Class1=React.Component;for(var ____Class1____Key in ____Class1){if(____Class1.hasOwnProperty(____Class1____Key)){Tabs[____Class1____Key]=____Class1[____Class1____Key];}}var ____SuperProtoOf____Class1=____Class1===null?null:____Class1.prototype;Tabs.prototype=Object.create(____SuperProtoOf____Class1);Tabs.prototype.constructor=Tabs;Tabs.__superConstructor__=____Class1;
 	  function Tabs(props) {"use strict";
 	    ____Class1.call(this,props);
@@ -198,8 +177,8 @@ webpackJsonp([0,1],[
 	    this.handleKeyDown = this.handleKeyDown.bind(this);
 	    this.prefixClsFn = prefixClsFn.bind(this);
 	  }
-	
-	  Tabs.prototype.$Tabs_getNextActiveKey=function() {"use strict";
+
+	  Object.defineProperty(Tabs.prototype,"$Tabs_getNextActiveKey",{writable:true,configurable:true,value:function() {"use strict";
 	    var activeKey = this.state.activeKey;
 	    var children = [];
 	    React.Children.forEach(this.props.children, function(c)  {
@@ -217,9 +196,9 @@ webpackJsonp([0,1],[
 	      }
 	    });
 	    return ret;
-	  };
-	
-	  Tabs.prototype.$Tabs_getPreviousActiveKey=function() {"use strict";
+	  }});
+
+	  Object.defineProperty(Tabs.prototype,"$Tabs_getPreviousActiveKey",{writable:true,configurable:true,value:function() {"use strict";
 	    var activeKey = this.state.activeKey;
 	    var children = [];
 	    React.Children.forEach(this.props.children, function(c) {
@@ -237,30 +216,30 @@ webpackJsonp([0,1],[
 	      }
 	    });
 	    return ret;
-	  };
-	
-	  Tabs.prototype.$Tabs_getTabPanes=function() {"use strict";
+	  }});
+
+	  Object.defineProperty(Tabs.prototype,"$Tabs_getTabPanes",{writable:true,configurable:true,value:function() {"use strict";
 	    var activeKey = this.state.activeKey;
 	    var children = this.props.children;
 	    var newChildren = [];
-	
+
 	    React.Children.forEach(children, function(child)  {
 	      newChildren.push(React.cloneElement(child, {
 	        active: activeKey === child.key,
 	        rootPrefixCls: this.state.prefixCls
 	      }));
 	    }.bind(this));
-	
+
 	    return newChildren;
-	  };
-	
-	  Tabs.prototype.$Tabs_getTabs=function() {"use strict";
+	  }});
+
+	  Object.defineProperty(Tabs.prototype,"$Tabs_getTabs",{writable:true,configurable:true,value:function() {"use strict";
 	    var children = this.props.children;
 	    var activeKey = this.state.activeKey;
 	    var rst = [];
 	    var prefixClsFn = this.prefixClsFn;
 	    var activeTabClassName = this.props.activeTabClassName;
-	
+
 	    React.Children.forEach(children, function(child) {
 	      var key = child.key;
 	      var cls = activeKey === key ? prefixClsFn('tab-active') + ' ' + activeTabClassName : '';
@@ -269,11 +248,11 @@ webpackJsonp([0,1],[
 	        React.createElement("a", null, child.props.tab)
 	      ));
 	    }.bind(this));
-	
+
 	    return rst;
-	  };
-	
-	  Tabs.prototype.handleTabClick=function(key) {"use strict";
+	  }});
+
+	  Object.defineProperty(Tabs.prototype,"handleTabClick",{writable:true,configurable:true,value:function(key) {"use strict";
 	    if (this.state.activeKey !== key) {
 	      this.setState({
 	        activeKey: key
@@ -282,9 +261,9 @@ webpackJsonp([0,1],[
 	        this.props.onChange(key);
 	      }
 	    }
-	  };
-	
-	  Tabs.prototype.handleKeyDown=function(e) {"use strict";
+	  }});
+
+	  Object.defineProperty(Tabs.prototype,"handleKeyDown",{writable:true,configurable:true,value:function(e) {"use strict";
 	    if (e.target !== React.findDOMNode(this)) {
 	      return;
 	    }
@@ -303,9 +282,9 @@ webpackJsonp([0,1],[
 	        this.handleTabClick(previousKey);
 	        break;
 	    }
-	  };
-	
-	  Tabs.prototype.render=function() {"use strict";
+	  }});
+
+	  Object.defineProperty(Tabs.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
 	    var tabs = this.$Tabs_getTabs();
 	    var tabPanes = this.$Tabs_getTabPanes();
 	    var cls = this.state.prefixCls;
@@ -320,26 +299,23 @@ webpackJsonp([0,1],[
 	        )
 	      )
 	    );
-	  };
-	
-	
+	  }});
+
+
 	Tabs.defaultProps = {
 	  prefixCls: 'rc-tabs',
 	  activeTabClassName: '',
 	  navClassName: '',
 	  contentClassName: ''
 	};
-	
+
 	Tabs.TabPane = TabPane;
-	
+
 	module.exports = Tabs;
 
 
 /***/ },
 /* 7 */
-/*!*************************************!*\
-  !*** ./~/style-loader/addStyles.js ***!
-  \*************************************/
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
@@ -362,20 +338,20 @@ webpackJsonp([0,1],[
 		}),
 		singletonElement = null,
 		singletonCounter = 0;
-	
+
 	module.exports = function(list, options) {
-		if(true) {
+		if(false) {
 			if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
 		}
-	
+
 		options = options || {};
 		// Force single-tag solution on IE9, which has a hard limit on the # of <style>
 		// tags it will allow on a page
 		if (typeof options.singleton === "undefined") options.singleton = isIE9();
-	
+
 		var styles = listToStyles(list);
 		addStylesToDom(styles, options);
-	
+
 		return function update(newList) {
 			var mayRemove = [];
 			for(var i = 0; i < styles.length; i++) {
@@ -398,7 +374,7 @@ webpackJsonp([0,1],[
 			}
 		};
 	}
-	
+
 	function addStylesToDom(styles, options) {
 		for(var i = 0; i < styles.length; i++) {
 			var item = styles[i];
@@ -420,7 +396,7 @@ webpackJsonp([0,1],[
 			}
 		}
 	}
-	
+
 	function listToStyles(list) {
 		var styles = [];
 		var newStyles = {};
@@ -438,7 +414,7 @@ webpackJsonp([0,1],[
 		}
 		return styles;
 	}
-	
+
 	function createStyleElement() {
 		var styleElement = document.createElement("style");
 		var head = getHeadElement();
@@ -446,10 +422,10 @@ webpackJsonp([0,1],[
 		head.appendChild(styleElement);
 		return styleElement;
 	}
-	
+
 	function addStyle(obj, options) {
 		var styleElement, update, remove;
-	
+
 		if (options.singleton) {
 			var styleIndex = singletonCounter++;
 			styleElement = singletonElement || (singletonElement = createStyleElement());
@@ -462,9 +438,9 @@ webpackJsonp([0,1],[
 				styleElement.parentNode.removeChild(styleElement);
 			};
 		}
-	
+
 		update(obj);
-	
+
 		return function updateStyle(newObj) {
 			if(newObj) {
 				if(newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap)
@@ -475,7 +451,7 @@ webpackJsonp([0,1],[
 			}
 		};
 	}
-	
+
 	function replaceText(source, id, replacement) {
 		var boundaries = ["/** >>" + id + " **/", "/** " + id + "<< **/"];
 		var start = source.lastIndexOf(boundaries[0]);
@@ -489,10 +465,10 @@ webpackJsonp([0,1],[
 			return source + wrappedReplacement;
 		}
 	}
-	
+
 	function applyToSingletonTag(styleElement, index, remove, obj) {
 		var css = remove ? "" : obj.css;
-	
+
 		if(styleElement.styleSheet) {
 			styleElement.styleSheet.cssText = replaceText(styleElement.styleSheet.cssText, index, css);
 		} else {
@@ -506,23 +482,23 @@ webpackJsonp([0,1],[
 			}
 		}
 	}
-	
+
 	function applyToTag(styleElement, obj) {
 		var css = obj.css;
 		var media = obj.media;
 		var sourceMap = obj.sourceMap;
-	
+
 		if(sourceMap && typeof btoa === "function") {
 			try {
 				css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(JSON.stringify(sourceMap)) + " */";
 				css = "@import url(\"data:text/css;base64," + btoa(css) + "\")";
 			} catch(e) {}
 		}
-	
+
 		if(media) {
 			styleElement.setAttribute("media", media)
 		}
-	
+
 		if(styleElement.styleSheet) {
 			styleElement.styleSheet.cssText = css;
 		} else {
@@ -536,9 +512,6 @@ webpackJsonp([0,1],[
 
 /***/ },
 /* 8 */
-/*!*************************************!*\
-  !*** ./~/css-loader/cssToString.js ***!
-  \*************************************/
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = function() {
@@ -560,4 +533,3 @@ webpackJsonp([0,1],[
 
 /***/ }
 ]);
-//# sourceMappingURL=simple.js.map
