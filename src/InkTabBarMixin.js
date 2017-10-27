7a2ed1348e38b7f2e1211dc5cf8ff04689103db0
afc163 <afc163@gmail.com>
2017-10-27 09:58:07 +0800

    better styleSheets judgement

--- src/InkTabBarMixin.js
+++ src/InkTabBarMixin.js
@@ -41,7 +41,7 @@ function componentDidUpdate(component, init) {
   // If there is no css rendered, don't generate the ink bar
   // fix https://github.com/ant-design/ant-design/issues/8001
   if (document.styleSheets.length === 0 ||
-      (document.styleSheets.length === 1 && document.styleSheets[0].rules.length === 0)) {
+      document.styleSheets.every(s => s.rules.length === 0)) {
     return;
   }
   const { styles } = component.props;
