0d304db9de4e0c91977c334626355c7cf6e84eed
贺业晟 <heyesheng@bytedance.com>
2019-09-23 19:50:03 +0800

    add unit test

--- src/TabBarTabsNode.js
+++ src/TabBarTabsNode.js
@@ -52,6 +52,7 @@ export default class TabBarTabsNode extends React.Component {
           key={key}
           style={ style }
           {...ref}
+          {...(typeof child.props.tab === 'string' ? {'data-tab': child.props.tab}: undefined)}
         >
           {child.props.tab}
         </div>
