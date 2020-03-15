836f222a37bf6b2ea2766ae6e4b09a683c6d984c
Filip Juncu <filip.juncu@gmail.com>
2020-03-15 19:38:24 +0100

    Added & updated tests & test snapshots

--- tests/swipe.spec.js
+++ tests/swipe.spec.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { mount, render } from 'enzyme';
 import { renderToJson } from 'enzyme-to-json';
-import Tabs, { TabPane } from '../src/';
+import Tabs, { TabPane } from '../src';
 import SwipeableTabContent from '../src/SwipeableTabContent';
 import SwipeableInkTabBar from '../src/SwipeableInkTabBar';
 
@@ -16,13 +16,11 @@ const contentStyle = {
 
 const makeTabPane = key => (
   <TabPane tab={`选项${key}`} key={key}>
-    <div style={contentStyle}>
-      {`选项${key}内容`}
-    </div>
+    <div style={contentStyle}>{`选项${key}内容`}</div>
   </TabPane>
 );
 
-const makeMultiTabPane = (count) => {
+const makeMultiTabPane = count => {
   const result = [];
   for (let i = 0; i < count; i++) {
     result.push(makeTabPane(i));
@@ -30,15 +28,22 @@ const makeMultiTabPane = (count) => {
   return result;
 };
 
-
-class NormoalTabs extends Component {
+class NormalTabs extends Component {
   render() {
     return (
       <div style={{ width: '750px', height: '1334px' }}>
         <Tabs
-          ref={root => { this.root = root; }}
+          ref={root => {
+            this.root = root;
+          }}
           defaultActiveKey="8"
-          renderTabBar={() => <SwipeableInkTabBar ref={tabBar => { this.tabBar = tabBar; }} />}
+          renderTabBar={() => (
+            <SwipeableInkTabBar
+              ref={tabBar => {
+                this.tabBar = tabBar;
+              }}
+            />
+          )}
           renderTabContent={() => <SwipeableTabContent />}
         >
           {makeMultiTabPane(11)}
@@ -49,20 +54,25 @@ class NormoalTabs extends Component {
 }
 describe('rc-swipeable-tabs', () => {
   it('should render Slider with correct DOM structure', () => {
-    const wrapper = render(<NormoalTabs />);
+    const wrapper = render(<NormalTabs />);
     expect(renderToJson(wrapper)).toMatchSnapshot();
   });
   it('create and nav should works', () => {
-    const wrapper = render(<NormoalTabs />);
+    const wrapper = render(<NormalTabs />);
     expect(wrapper.find('.rc-tabs').length).toBe(1);
     expect(wrapper.find('.rc-tabs-tab').length).toBe(11);
   });
 
   it('default active should works', () => {
-    const wrapper = mount(<NormoalTabs />);
+    const wrapper = mount(<NormalTabs />);
     expect(wrapper.find('.rc-tabs-tab').length).toBe(11);
     expect(wrapper.instance().root.state.activeKey).toBe('8');
-    expect(wrapper.find('.rc-tabs-tab').at(8).hasClass('rc-tabs-tab-active')).toBe(true);
+    expect(
+      wrapper
+        .find('.rc-tabs-tab')
+        .at(8)
+        .hasClass('rc-tabs-tab-active'),
+    ).toBe(true);
   });
 
   it('onChange and onTabClick should works', () => {
@@ -76,7 +86,7 @@ describe('rc-swipeable-tabs', () => {
         onChange={handleChange}
       >
         {makeMultiTabPane(11)}
-      </Tabs>
+      </Tabs>,
     );
     const targetTab = wrapper.find('.rc-tabs-tab').at(6);
     targetTab.simulate('click');
