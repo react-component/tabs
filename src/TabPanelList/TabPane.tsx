b32fbfc2a1497acdb7dda4f2668715e14d87f307
zombiej <smith3816@gmail.com>
2020-05-20 18:02:01 +0800

    visited render

--- src/TabPanelList/TabPane.tsx
+++ src/TabPanelList/TabPane.tsx
@@ -9,7 +9,20 @@ export interface TabPaneProps {
   active: boolean;
 }
 
-export default function TabPane({ prefixCls, id, active, tab: { key, children } }: TabPaneProps) {
+export default function TabPane({
+  prefixCls,
+  id,
+  active,
+  tab: { key, children, forceRender },
+}: TabPaneProps) {
+  const [visited, setVisited] = React.useState(forceRender);
+
+  React.useEffect(() => {
+    if (active) {
+      setVisited(true);
+    }
+  }, [active]);
+
   return (
     <div
       id={id && `${id}-panel-${key}`}
@@ -20,7 +33,7 @@ export default function TabPane({ prefixCls, id, active, tab: { key, children } 
       style={{ visibility: active ? 'visible' : 'hidden' }}
       className={classNames(`${prefixCls}-tabpane`, active && `${prefixCls}-tabpane-active`)}
     >
-      {children}
+      {(visited || forceRender) && children}
     </div>
   );
 }
