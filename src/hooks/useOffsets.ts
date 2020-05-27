5fab2e62ca62b38242f3d3ebbe0833100b47d07f
zombiej <smith3816@gmail.com>
2020-05-27 18:59:44 +0800

    fix rtl

--- src/hooks/useOffsets.ts
+++ src/hooks/useOffsets.ts
@@ -1,9 +1,14 @@
 import { useMemo } from 'react';
 import { TabSizeMap, TabOffsetMap, Tab, TabOffset } from '../interface';
 
+const DEFAULT_SIZE = { width: 0, height: 0, left: 0, top: 0 };
+
 export default function useOffsets(tabs: Tab[], tabSizes: TabSizeMap, holderScrollWidth: number) {
   return useMemo(() => {
     const map: TabOffsetMap = new Map();
+
+    const lastOffset = tabSizes.get(tabs[0]?.key) || DEFAULT_SIZE;
+    const rightOffset = lastOffset.left + lastOffset.width;
 
     for (let i = 0; i < tabs.length; i += 1) {
       const { key } = tabs[i];
@@ -11,13 +16,13 @@ export default function useOffsets(tabs: Tab[], tabSizes: TabSizeMap, holderScro
 
       // Reuse last one when not exist yet
       if (!data) {
-        data = tabSizes.get(tabs[i - 1]?.key) || { width: 0, height: 0, left: 0, top: 0 };
+        data = tabSizes.get(tabs[i - 1]?.key) || DEFAULT_SIZE;
       }
 
       const entity = (map.get(key) || { ...data }) as TabOffset;
 
       // Right
-      entity.right = holderScrollWidth - (entity.left + entity.width);
+      entity.right = rightOffset - entity.left - entity.width;
 
       // Update entity
       map.set(key, entity);
