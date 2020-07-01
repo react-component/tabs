bc6238c57ca6c41e9a7bd7919f16f11594ee504b
Yu Mao <maoyu960320@hotmail.com>
2020-07-01 22:01:04 +0800

    Revert "memoization"
    This reverts commit cbe5af2e436bf6c0a43752bada3e1a3a05eda8d9.

--- src/TabNavList/index.tsx
+++ src/TabNavList/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useRef, useEffect, useMemo } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames';
 import raf from 'raf';
 import ResizeObserver from 'rc-resize-observer';
@@ -89,10 +89,6 @@ function TabNavList(props: TabNavListProps, ref: React.Ref<HTMLDivElement>) {
 
   const [tabSizes, setTabSizes] = useRafState<TabSizeMap>(new Map());
   const tabOffsets = useOffsets(tabs, tabSizes, wrapperScrollWidth);
-
-  const tabsKeyOrder = useMemo(() => {
-    return tabs.map(tab => tab.key).join('_')
-  }, [tabs])
 
   // ========================== Util =========================
   const operationsHiddenClassName = `${prefixCls}-nav-operations-hidden`;
@@ -359,7 +355,7 @@ function TabNavList(props: TabNavListProps, ref: React.Ref<HTMLDivElement>) {
   // Should recalculate when rtl changed
   useEffect(() => {
     onListHolderResize();
-  }, [rtl, tabBarGutter, activeKey, tabsKeyOrder]);
+  }, [rtl, tabBarGutter, activeKey, tabs.map((tab) => tab.key).join('_')]);
 
   // ========================= Render ========================
   const hasDropdown = !!hiddenTabs.length;
