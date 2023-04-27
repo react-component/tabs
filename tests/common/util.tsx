808b9a6dbb67606258bf695db9909935b2ef9b15
MadCcc <1075746765@qq.com>
2023-04-27 17:22:16 +0800

    chore: bump rc-dropdown to 4.1.0 (#661)
    * chore: bump rc-dropdown to 4.1.0
    
    * test: update

--- tests/common/util.tsx
+++ tests/common/util.tsx
@@ -158,3 +158,24 @@ export const triggerResize = (container: Element) => {
     onEsResize([{ target } as ResizeObserverEntry]);
   });
 };
+
+/**
+ * Wait for a time delay. Will wait `advanceTime * times` ms.
+ *
+ * @param advanceTime Default 1000
+ * @param times Default 20
+ */
+export async function waitFakeTimer(advanceTime = 1000, times = 20) {
+  for (let i = 0; i < times; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    await act(async () => {
+      await Promise.resolve();
+
+      if (advanceTime > 0) {
+        jest.advanceTimersByTime(advanceTime);
+      } else {
+        jest.runAllTimers();
+      }
+    });
+  }
+}
