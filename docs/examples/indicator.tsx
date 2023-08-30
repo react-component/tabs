c968dee3cf96c21f8c768698f287c9520ec96985
MadCcc <1075746765@qq.com>
2023-08-30 15:03:22 +0800

    refactor: rename indicatorLength with indicatorSize

--- docs/examples/indicator.tsx
+++ docs/examples/indicator.tsx
@@ -29,7 +29,7 @@ export default () => {
 
   return (
     <React.StrictMode>
-      <Tabs tabBarExtraContent="extra" items={items} getIndicatorLength={(origin) => origin - 16} />
+      <Tabs tabBarExtraContent="extra" items={items} getindicatorSize={(origin) => origin - 16} />
     </React.StrictMode>
   );
 };
